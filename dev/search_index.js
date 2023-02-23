9b6612425421094bda353182a126ba16945376fa
Documenter.jl <documenter@juliadocs.github.io>
2023-02-23 13:10:46 +0000

    build based on 4f09832

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NS2DBenchmarkSolver.jl-Documentation","page":"Home","title":"NS2DBenchmarkSolver.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = NS2DBenchmarkSolver\nDocTestSetup = quote\n    using NS2DBenchmarkSolver \nend","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NS2DBenchmarkSolver]","category":"page"},{"location":"#NS2DBenchmarkSolver.sum_values-Tuple{Any, Any}","page":"Home","title":"NS2DBenchmarkSolver.sum_values","text":"sum_values(x,y)\n\nReturns sum of x and y.\n\nExample\n\njulia> sum_values(1, 2)\n3\n\n\n\n\n\n","category":"method"}]
+}
