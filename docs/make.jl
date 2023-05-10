4bbe084a1777ef7fb356082fc501c6b9f3205300
Moritz Waldleben <moritz.waldleben@gmail.com>
2023-05-10 16:41:34 +0200

    Fix module name typo

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ using Documenter, FourSidedCavityFlow
 
 DocMeta.setdocmeta!(FourSidedCavityFlow, :DocTestSetup, :(using FourSidedCavityFlow); recursive = true)
 
-makedocs(sitename = "FourSidedCavityFlow.jl", modules = [FourSidedavityFlow], pages = ["Home" => "index.md"])
+makedocs(sitename = "FourSidedCavityFlow.jl", modules = [FourSidedCavityFlow], pages = ["Home" => "index.md"])
 
 deploydocs(repo = "github.com/morwald/FourSidedCavityFlow.jl.git")
