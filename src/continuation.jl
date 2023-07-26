7e9ced31407a6eba1b56b61ea2b20d7ff3b4cc71
Moritz Waldleben <moritz.waldleben@gmail.com>
2023-07-26 13:42:04 +0200

    One function for continuation with or without linear stability analysis

--- src/continuation.jl
+++ src/continuation.jl
@@ -1,98 +1,38 @@
-function continuation_arclength(foldercont, Ψi, p::CavityStruct, Re_start, ΔRe, Re_steps)
+function continuation_arclength(foldercont, Ψ1, Ψ2, p::CavityStruct, Re1, Re2, Re_steps; lsa = false)
     @unpack n, scl = p.params
 
     sol = Array{Float64}(undef, (Re_steps + 1, n + 1, n + 1))
     Re_series = Vector{Float64}(undef, Re_steps + 1)
 
-    # Write header
-    fileresults = "$(foldercont)/results.csv"
-    write_header(fileresults)
-
-    p.params.Re = Re_start
-    dim = (n - 3) * (n - 3)
-    @inbounds u0 = reshape(Ψi[3:(n - 1), 3:(n - 1)], dim)
-    u1, _, _ = newton(f!, u0, p)
-    Ψ1 = constructBC(u1, p)
     sol[1, :, :] = Ψ1
-    Re_series[1] = Re_start
-    save_result(fileresults, Ψ1, 0, 0, 0.0, p)
-
-    p.params.Re = Re_start + ΔRe
-    u2, _, _ = newton(f!, u0, p)
-    Ψ2 = constructBC(u2, p)
-    sol[1, :, :] = Ψ2
-    Re_series[1] = Re_start + ΔRe
-    save_result(fileresults, Ψ2, 1, 0, 0.0, p)
-
-    # Augmented system
-    x1 = [u1; p.params.Re / scl]
-    x2 = [u2; p.params.Re / scl]
-
-    # Step size norm
-    s = norm(x2 - x1)
-
-    # Setup continuation cache
-    x = similar(x1)
-    fx = similar(x1)
-    dx = similar(x1)
-    v = similar(x1)
-    xp = similar(x1)
-
-    dim = (n - 3) * (n - 3)
-    xi = zeros(dim)
-    fxi = similar(xi)
-    fxi_Re = similar(xi)
-
-    J = zeros(dim, dim)
-    J2 = zeros(dim + 1, dim + 1)
-    jac_cache = FiniteDiff.JacobianCache(xi)
-
-    cont_cache = (x, fx, dx, v, xp, xi, fxi, fxi_Re, J, J2, jac_cache)
-
-    for i in 2:(Re_steps + 1)
-        time = @elapsed tmp, iter, _ = newton_continuation(f!, x1, x2, s, p, cont_cache)
-
-        x1 .= x2
-        x2 .= tmp
-
-        p.params.Re = tmp[end] * scl
-        Ψ = constructBC(tmp[1:(end - 1)], p)
-
-        Re_series[i] = p.params.Re
-        sol[i, :, :] = Ψ
-        save("$foldercont/psis.jld2", "sol", sol[1:i, :, :], "Re_series", Re_series[1:i])
-        save_result(fileresults, Ψ, i, iter, time, p)
-    end
-
-    return sol, Re_series
-end
-
-function continuation_arclength_lsa(folderlsa, name, Ψi1, Ψi2, p::CavityStruct, Re1, Re2, Re_steps)
-    @unpack n, scl = p.params
-
-    sol = Array{Float64}(undef, (Re_steps + 1, n + 1, n + 1))
-    Re_series = Vector{Float64}(undef, Re_steps + 1)
-    lambdas = Array{Float64}(undef, (Re_steps + 1, (n + 1)*(n + 1)))
-
-    # Write header
-    filelsa = "$folderlsa/results_$(name).csv"
-    write_header_lsa(filelsa)
-
-    p.params.Re = Re1
-    @inbounds u1 = reshape(Ψi1[3:(n - 1), 3:(n - 1)], (n - 3) * (n - 3))
-    lsa_time = @elapsed lambda = linearstability_lambdas(u1, p)
     Re_series[1] = Re1
-    sol[1, :, :] = Ψi1
-    lambdas[1, :] = lambda
-    save_result_lsa(filelsa, Ψi1, 0, lambdas, lsa_time, 0, 0, p)
+    @inbounds u1 = reshape(Ψ1[3:(n - 1), 3:(n - 1)], (n - 3) * (n - 3))
 
-    p.params.Re = Re2
-    @inbounds u2 = reshape(Ψi2[3:(n - 1), 3:(n - 1)], (n - 3) * (n - 3))
-    lsa_time = @elapsed lambdas = linearstability_lambdas(u2, p)
-    Re_series[2] = Re1
-    sol[2, :, :] = Ψi2
-    lambdas[2, :] = lambda
-    save_result_lsa(filelsa, Ψi2, 1, lambdas, lsa_time, 0, 0, p)
+    sol[2, :, :] = Ψ2
+    Re_series[2] = Re2
+    @inbounds u2 = reshape(Ψ2[3:(n - 1), 3:(n - 1)], (n - 3) * (n - 3))
+
+    if lsa == true
+        lambdas = Array{Float64}(undef, (Re_steps + 1, (n + 1)*(n + 1)))
+
+        lsa_time = @elapsed lambda = linearstability_lambdas(u1, p)
+        lambdas[1, :] = lambda
+
+        lsa_time = @elapsed lambda = linearstability_lambdas(u2, p)
+        lambdas[2, :] = lambda
+
+        fileresults = "$(foldercont)/results.csv"
+        write_header_lsa(fileresults)
+
+        save_result_lsa(fileresults, Ψ1, 0, lambdas, lsa_time, 0, 0, p)
+        save_result_lsa(fileresults, Ψ2, 1, lambdas, lsa_time, 0, 0, p)
+    else
+        fileresults = "$(foldercont)/results.csv"
+        write_header(fileresults)
+
+        save_result(fileresults, Ψ1, 0, 0, 0.0, p)
+        save_result(fileresults, Ψ2, 1, 0, 0.0, p)
+    end
 
     # Augmented system
     x1 = [u1; p.params.Re / scl]
@@ -128,33 +68,43 @@ function continuation_arclength_lsa(folderlsa, name, Ψi1, Ψi2, p::CavityStruct
         p.params.Re = tmp[end] * scl
         Ψ = constructBC(tmp[1:(end - 1)], p)
 
-        lsa_time = @elapsed lambda = linearstability_lambdas(tmp[1:(end - 1)], p)
-
         Re_series[i] = p.params.Re
         sol[i, :, :] = Ψ
-        lambdas[i, :] = lambda
-        save("$folderlsa/psis.jld2", "sol", sol[1:i, :, :], "lambdas", lambdas[1:i, :, :], "Re_series", Re_series[1:i])
-        save_result_lsa(filelsa, Ψ, i, lambdas, lsa_time, iter, newton_time, p)
+
+        if lsa == true
+            lsa_time = @elapsed lambda = linearstability_lambdas(tmp[1:(end - 1)], p)
+            lambdas[i, :] = lambda
+
+            save("$foldercont/psis.jld2", "sol", sol[1:i, :, :], "lambdas", lambdas[1:i, :, :], "Re_series", Re_series[1:i])
+            save_result_lsa(fileresults, Ψ, i, lambdas, lsa_time, iter, newton_time, p)
+        else
+            save("$foldercont/psis.jld2", "sol", sol[1:i, :, :], "Re_series", Re_series[1:i])
+            save_result(fileresults, Ψ, i, iter, time, p)
+        end
     end
-    return
+
+    if lsa == true
+        sol, lambdas, Re_series
+    else
+        sol, Re_series
+    end
 end
 
-function continuation_arclength_lsa(folderlsa, name, Ψi, p::CavityStruct, Re_start, ΔRe,
-    Re_steps)
+function continuation_arclength(foldercont, Ψi, p::CavityStruct, Re_start, ΔRe, Re_steps; lsa = false)
     @unpack n = p.params
 
     Re1 = Re_start
     p.params.Re = Re1
     @inbounds u0 = reshape(Ψi[3:(n - 1), 3:(n - 1)], (n - 3) * (n - 3))
     u1, _, _ = newton(f!, u0, p)
-    Ψi1 = constructBC(u1, p)
+    Ψ1 = constructBC(u1, p)
 
     Re2 = Re_start + ΔRe
     p.params.Re = Re2
     u2, _, _ = newton(f!, u0, p)
-    Ψi2 = constructBC(u2, p)
+    Ψ2 = constructBC(u2, p)
 
-    return continuation_arclength_lsa(folderlsa, name, Ψi1, Ψi2, p, Re1, Re2, Re_steps)
+    return continuation_arclength(foldercont, Ψ1, Ψ2, p, Re1, Re2, Re_steps; lsa)
 end
 
 function newton_continuation(f!, x1, x2, s, p::CavityStruct, cont_cache;
@@ -246,13 +196,6 @@ function newton_continuation(f!, x1, x2, s, p::CavityStruct; abstol = 1e-10, max
         maxiters = maxiters)
 
     return x, iter, tol
-end
-
-function saveΨ(folder, Ψ, step, Re)
-    writedlm("$folder/psi_step$(@sprintf("%03d", step))_Re$(@sprintf("%07.3f", Re)).txt",
-        Ψ)
-
-    return nothing
 end
 
 function save_result(fileresults, Ψ, step, iter, time, p)
